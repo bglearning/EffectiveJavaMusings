44f33c7af32745a63c05340396e4cb85594154b6
Bijay Gurung <bijay@fusemachines.com>
2017-01-12 23:28:35 +0545

    Add Item 4; Enforce Noninstantiability

---
+++ src/main/java/ejm/chapter02/item04/KitchenUtils.java
@@ -0,0 +1,9 @@
+package ejm.chapter02.item04;
+
+public class KitchenUtils {
+
+    // Suppress default constructor for non-instantiability
+    private KitchenUtils() {
+        throw new AssertionError();
+    }
+}
