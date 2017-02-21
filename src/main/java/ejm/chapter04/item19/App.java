6f7b47c97b7e9d550b17d625dae0cb1ccd731c04
Bijay Gurung <bijay@fusemachines.com>
2017-02-21 22:15:43 +0545

    Add Item 19; Use Interfaces only to define types
    Use Constant utility classes to store constants.

---
+++ src/main/java/ejm/chapter04/item19/App.java
@@ -0,0 +1,11 @@
+package ejm.chapter04.item19;
+
+import static ejm.chapter04.item19.PhysicalConstants.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Mass of 100,000 electrons is: " + 
+                100000 * ELECTRON_MASS);
+    }
+}
---
+++ src/main/java/ejm/chapter04/item19/PhysicalConstants.java
@@ -0,0 +1,12 @@
+package ejm.chapter04.item19;
+
+public class PhysicalConstants {
+    private PhysicalConstants() {
+    }
+
+    public static final double AVOGADROS_NUMBER = 6.02214199e23;
+
+    public static final double BOLTZMANN_CONSTANT = 1.3806503e-23;
+
+    public static final double ELECTRON_MASS = 9.10938188e-31;
+}
