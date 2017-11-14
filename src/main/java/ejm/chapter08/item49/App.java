4dcf7177eb1ec25f34db5801db9c9f2423905a35
Bijay Gurung <bijay@fusemachines.com>
2017-11-14 06:55:57 +0545

    Add Item 49; Prefer primitives to boxed primitives

---
+++ src/main/java/ejm/chapter08/item49/App.java
@@ -0,0 +1,13 @@
+package ejm.chapter08.item49;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        Integer a = new Integer(1);
+        Integer b = new Integer(1);
+
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+    }
+}
